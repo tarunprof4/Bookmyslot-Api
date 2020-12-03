71d3e6b6fe9186f155952a56ffc71d3c692751f7
tarun aggarwal <tarun.aggarwal4@gmail.com>
2020-12-03 23:20:10 +0530

    Added Swagger

--- Customers/Bookmyslot.Api.Customers/Startup.cs
+++ Customers/Bookmyslot.Api.Customers/Startup.cs
@@ -32,6 +32,8 @@ namespace Bookmyslot.Api.Customers
             CustomerInjection.CustomerBusinessInjections(services);
             CustomerInjection.CustomerRepositoryInjections(services);
             services.AddControllers();
+
+            services.AddSwaggerGen();
         }
 
       
@@ -45,6 +47,16 @@ namespace Bookmyslot.Api.Customers
             }
 
             app.UseHttpsRedirection();
+
+
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseRouting();
 
