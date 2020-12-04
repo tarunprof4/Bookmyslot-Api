e70ff0d54fd3943946632afc4e16978269d8b9c5
tarun aggarwal <tarun.aggarwal4@gmail.com>
2020-12-04 15:02:48 +0530

    Common Classes

---
+++ Common/Bookmyslot.Api.Common/ResultType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookmyslot.Api.Common
+{
+    public enum ResultType
+    {
+        ValidationError,
+        Error,
+        Success,
+    }
+}
