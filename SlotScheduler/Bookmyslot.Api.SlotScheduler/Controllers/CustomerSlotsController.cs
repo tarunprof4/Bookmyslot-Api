41243a4d9b5decddf6a0f239b4962e283190b233
tarun aggarwal <tarun.aggarwal4@gmail.com>
2020-12-04 18:17:29 +0530

    Slot Scheduler

---
+++ SlotScheduler/Bookmyslot.Api.SlotScheduler/Controllers/CustomerSlotsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Bookmyslot.Api.SlotScheduler.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerSlotsController : ControllerBase
+    {
+        // GET: api/<CustomerSlotsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CustomerSlotsController>/5
+        [HttpGet("{email}")]
+        public string Get(string email)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomerSlotsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CustomerSlotsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomerSlotsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SlotScheduler/Bookmyslot.Api.SlotScheduler/Controllers/SlotSchedulerController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Bookmyslot.Api.SlotScheduler.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SlotSchedulerController : ControllerBase
+    {
+        // GET: api/<SlotSchedulerController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SlotSchedulerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SlotSchedulerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SlotSchedulerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SlotSchedulerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
