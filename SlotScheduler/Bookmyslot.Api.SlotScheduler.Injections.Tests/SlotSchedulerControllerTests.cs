d64fe48cde95992b40ec36d1c4d567f70e250376
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-01-06 12:57:42 +0530

    IOC Tests

---
+++ SlotScheduler/Bookmyslot.Api.SlotScheduler.Injections.Tests/SlotSchedulerControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+
+namespace Bookmyslot.Api.SlotScheduler.Injections.Tests
+{
+    public class SlotSchedulerControllerTests
+    {
+        private IServiceProvider serviceProvider;
+
+        [SetUp]
+        public void Setup()
+        {
+            var webHost = WebHost.CreateDefaultBuilder().UseStartup<Startup>().Build();
+            serviceProvider = webHost.Services;
+            var configuration = serviceProvider.GetService<IConfiguration>();
+            var Startup = new Startup(configuration);
+        }
+
+        [Test]
+        public void StartupTest()
+        {
+            //var slotBusiness = serviceProvider.GetService<ISlotBusiness>();
+            //var controller = new SlotController(slotBusiness);
+
+            //Assert.IsNotNull(slotBusiness);
+            //Assert.IsNotNull(controller);
+        }
+    }
+}
