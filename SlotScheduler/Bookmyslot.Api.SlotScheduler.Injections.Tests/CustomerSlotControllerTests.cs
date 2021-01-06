31ea9578f9a296db2103d591f9af5f3f2eed7fc8
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-01-06 13:01:39 +0530

    ControllerTests

---
+++ SlotScheduler/Bookmyslot.Api.SlotScheduler.Injections.Tests/CustomerSlotControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Bookmyslot.Api.SlotScheduler.Contracts.Interfaces;
+using Bookmyslot.Api.SlotScheduler.Controllers;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+namespace Bookmyslot.Api.SlotScheduler.Injections.Tests
+{
+    public class CustomerSlotControllerTests
+    {
+        private IServiceProvider serviceProvider;
+
+        [SetUp]
+        public void Setup()
+        {
+            var webHost = WebHost.CreateDefaultBuilder().UseStartup<Startup>().Build();
+            serviceProvider = webHost.Services;
+            var configuration = serviceProvider.GetService<IConfiguration>();
+            var Startup = new Startup(configuration);
+        }
+
+        [Test]
+        public void StartupTest()
+        {
+            var customerSlotBusiness = serviceProvider.GetService<ICustomerSlotBusiness>();
+            var controller = new CustomerSlotController(customerSlotBusiness);
+
+            Assert.IsNotNull(customerSlotBusiness);
+            Assert.IsNotNull(controller);
+        }
+    }
+}
