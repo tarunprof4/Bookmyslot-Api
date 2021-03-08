dd71e46e98fed6a5b7541161ff52d799a09ae173
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-03-08 11:03:37 +0400

    FaceBook Login

--- Authentication/Bookmyslot.Api.Authentication.Facebook/FaceBookTokenValidator.cs
+++ Authentication/Bookmyslot.Api.Authentication.Facebook/FaceBookTokenValidator.cs
@@ -1,8 +1,12 @@
 ﻿using Bookmyslot.Api.Authentication.Common;
 using Bookmyslot.Api.Authentication.Common.Configuration;
+using Bookmyslot.Api.Authentication.Common.Constants;
 using Bookmyslot.Api.Authentication.Common.Interfaces;
 using Bookmyslot.Api.Common.Contracts;
+using Bookmyslot.Api.Common.Contracts.Constants;
+using Serilog;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Bookmyslot.Api.Authentication.Facebook
@@ -17,7 +21,30 @@ namespace Bookmyslot.Api.Authentication.Facebook
         }
         public Task<Response<SocialCustomerModel>> ValidateToken(string token)
         {
-            throw new NotImplementedException();
+            //try
+            //{
+            //    var validationSettings = new GoogleJsonWebSignature.ValidationSettings();
+            //    validationSettings.Audience = new List<string>() { this.authenticationConfiguration.GoogleClientId };
+            //    var validPayload = await GoogleJsonWebSignature.ValidateAsync(token, validationSettings);
+
+            //    return new Response<SocialCustomerModel>() { Result = CreateSocialCustomerModel(validPayload) };
+            //}
+            //catch (Exception ex)
+            //{
+            //    Log.Error(ex, string.Empty);
+            //    return Response<SocialCustomerModel>.ValidationError(new List<string>() { AppBusinessMessagesConstants.LoginFailed });
+            //}
+        }
+
+        private SocialCustomerModel CreateSocialCustomerModel()
+        {
+            return new SocialCustomerModel()
+            {
+                //FirstName = payload.GivenName,
+                //LastName = payload.FamilyName,
+                //Email = payload.Email,
+                Provider = LoginConstants.ProviderFacebook
+            };
         }
     }
 }
