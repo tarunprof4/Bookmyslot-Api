53458748e4a203f4b255527d94026487b82be688
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-03-09 13:03:19 +0400

    Added Register Table

--- Customers/Bookmyslot.Api.Customers.Repositories/Queries/CustomerTableQueries.cs
+++ Customers/Bookmyslot.Api.Customers.Repositories/Queries/CustomerTableQueries.cs
@@ -1,21 +1,23 @@
-﻿namespace Bookmyslot.Api.Customers.Repositories.Queries
+﻿using Bookmyslot.Api.Common.Database.Constants;
+
+namespace Bookmyslot.Api.Customers.Repositories.Queries
 {
 
     public class CustomerTableQueries
     {
-        public const string UpdateProfileSettingQuery = @"UPDATE Customer SET 
+        public const string UpdateProfileSettingQuery = @"UPDATE"+ " "+ TableNameConstants.Register + " " + @"SET 
  FirstName = @FirstName,  LastName = @LastName, Gender=@Gender, ModifiedDateUtc =@ModifiedDateUtc
  WHERE UniqueId=@customerId";
 
-        public const string GetCustomersByCustomerIdsQuery = @"select UniqueId, Email, FirstName, LastName, BioHeadLine from Customer where UniqueId IN @CustomerIds";
+        public const string GetCustomersByCustomerIdsQuery = @"select UniqueId, Email, FirstName, LastName, BioHeadLine from" + " " + TableNameConstants.Register + " " + "where UniqueId IN @CustomerIds";
 
-        public const string GetCustomerByEmailQuery = @"select UniqueId, Email, FirstName, LastName, BioHeadLine from Customer where Email = @Email";
+        public const string GetCustomerByEmailQuery = @"select UniqueId, Email, FirstName, LastName, BioHeadLine from" + " " + TableNameConstants.Register + " " + "where Email = @Email";
 
-        public const string GetCustomerIdByEmailQuery = @"select UniqueId from Customer where Email = @Email";
+        public const string GetCustomerIdByEmailQuery = @"select UniqueId from" + " " + TableNameConstants.Register + " " + "where Email = @Email";
 
-        public const string GetCustomerByIdQuery = @"select UniqueId, Email, FirstName, LastName, BioHeadLine from Customer where UniqueId = @CustomerId";
+        public const string GetCustomerByIdQuery = @"select UniqueId, Email, FirstName, LastName, BioHeadLine from" + " " + TableNameConstants.Register + " " + "where UniqueId = @CustomerId";
 
-        public const string GetProfileSettingsByCustomerIdQuery = @"select Email, FirstName, LastName, Gender from Customer where UniqueId=@customerId";
+        public const string GetProfileSettingsByCustomerIdQuery = @"select Email, FirstName, LastName, Gender from" + " " + TableNameConstants.Register + " " + "where UniqueId=@customerId";
 
     }
 }
