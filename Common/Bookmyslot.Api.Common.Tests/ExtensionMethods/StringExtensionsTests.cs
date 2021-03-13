bb7950ae3acfe2a035a3ec107dae2dd6c4041c39
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-03-13 10:20:04 -0800

    Test Cases Passed

--- Common/Bookmyslot.Api.Common.Tests/ExtensionMethods/StringExtensionsTests.cs
+++ Common/Bookmyslot.Api.Common.Tests/ExtensionMethods/StringExtensionsTests.cs
@@ -1,5 +1,7 @@
-﻿using Bookmyslot.Api.Common.ExtensionMethods;
+﻿using Bookmyslot.Api.Common.Contracts.Constants;
+using Bookmyslot.Api.Common.ExtensionMethods;
 using NUnit.Framework;
+using System;
 
 namespace Bookmyslot.Api.Common.Tests.ExtensionMethods
 {
@@ -13,9 +15,9 @@ namespace Bookmyslot.Api.Common.Tests.ExtensionMethods
         {
         }
 
-        [TestCase("1-1-2000")]
+        [TestCase("01-01-2000")]
         [TestCase("12-12-2000")]
-        [TestCase("12-13-2000")]
+        [TestCase("13-11-2000")]
         public void IsDateValid_ValidDateToCheck_ReturnsTrueResponse(string validDate)
         {
             var isDateValid = validDate.isDateValid();
@@ -23,7 +25,7 @@ namespace Bookmyslot.Api.Common.Tests.ExtensionMethods
         }
 
 
-        [TestCase("1-13-2000")]
+        [TestCase("01-13-2000")]
         [TestCase("12-13-2000")]
         public void IsDateValid_InValidDateToCheck_ReturnsFalseResponse(string inValidDate)
         {
