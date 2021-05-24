c2cf5fe75e2b6dd580a3ffae8e5c137d3bb5650d
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-05-24 16:12:57 +0530

    Encryption Messages

---
+++ Common/Bookmyslot.Api.Common.Tests/EncryptionTests/SymmetryEncryptionTests.cs
@@ -0,0 +1,35 @@
+﻿using Bookmyslot.Api.Common.Encryption;
+using Bookmyslot.Api.Common.Encryption.Interfaces;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookmyslot.Api.Common.Tests.EncryptionTests
+{
+    public class SymmetryEncryptionTests
+    {
+        private AesSymmetricEncryption aesSymmetricEncryption;
+        private IRandomNumberGenerator randomNumberGenerator;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            randomNumberGenerator = new RandomNumberGenerator();
+            aesSymmetricEncryption = new AesSymmetricEncryption(randomNumberGenerator);
+        }
+
+        [TestCase("test", "test")]
+        [TestCase("", "")]
+        [TestCase(null, "")]
+        [TestCase("123", "123")]
+        [TestCase("asdjkhsdjkhasdjkasdhjk", "asdjkhsdjkhasdjkasdhjk")]
+        public void EncryptMessage_DecryptMessageBack_ReturnsOriginalMessage(string input, string output)
+        {
+            var encryptedMessage = this.aesSymmetricEncryption.Encrypt(input);
+            var decryptedMessage = this.aesSymmetricEncryption.Decrypt(encryptedMessage);
+            Assert.AreEqual(output, decryptedMessage);
+        }
+    }
+}
