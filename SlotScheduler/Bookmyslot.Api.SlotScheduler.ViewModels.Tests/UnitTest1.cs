c86ab8c329d5333595b35c4f5ffdda1034c240f9
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-05-28 22:23:31 +0530

    Added Adaptor Unit Tests

---
+++ SlotScheduler/Bookmyslot.Api.SlotScheduler.ViewModels.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Bookmyslot.Api.SlotScheduler.ViewModels.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
