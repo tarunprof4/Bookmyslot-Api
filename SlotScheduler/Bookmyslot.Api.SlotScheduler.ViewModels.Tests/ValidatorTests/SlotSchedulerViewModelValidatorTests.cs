8d9aead2e0ede095a564f392a2969ab5af18b829
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-05-30 17:33:28 +0530

    Validators Tests

---
+++ SlotScheduler/Bookmyslot.Api.SlotScheduler.ViewModels.Tests/ValidatorTests/SlotSchedulerViewModelValidatorTests.cs
@@ -0,0 +1,34 @@
+﻿using Bookmyslot.Api.SlotScheduler.ViewModels.Validations;
+using FluentValidation;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookmyslot.Api.SlotScheduler.ViewModels.Tests.ValidatorTests
+{
+    [TestFixture]
+    public class SlotSchedulerViewModelValidatorTests
+    {
+        private IValidator<SlotSchedulerViewModel> slotSchedulerViewModelValidator;
+
+        [SetUp]
+        public void Setup()
+        {
+            slotSchedulerViewModelValidator = new SlotSchedulerViewModelValidator();
+        }
+
+
+        [Test]
+        public void CreateBookAvailableSlotViewModel_EmptyBookAvailableSlotModel_ReturnsEmptyBookAvailableSlotViewModel()
+        {
+            var messages = slotSchedulerViewModelValidator.Validate(null);
+        }
+
+       
+     
+
+     
+
+    }
+}
