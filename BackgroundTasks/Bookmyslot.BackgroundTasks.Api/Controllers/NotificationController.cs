906dc2e440c514dad9647f3953e068e93d7ef020
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-06-06 17:39:03 +0530

    BaseApiController

--- BackgroundTasks/Bookmyslot.BackgroundTasks.Api/Controllers/NotificationController.cs
+++ BackgroundTasks/Bookmyslot.BackgroundTasks.Api/Controllers/NotificationController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bookmyslot.Api.Web.Common;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bookmyslot.BackgroundTasks.Api.Controllers
 {
     [Route("api/[controller]")]
+    [Produces("application/json")]
+    [Consumes("application/json")]
     [ApiController]
-    public class NotificationController : ControllerBase
+    public class NotificationController : BaseApiController
     {
         public NotificationController()
         {
