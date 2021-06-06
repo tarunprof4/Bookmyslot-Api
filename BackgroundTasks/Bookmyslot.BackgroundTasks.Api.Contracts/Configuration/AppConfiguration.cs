f29a002f347118534a14d72f0f3cf9d1b9bea7f7
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-06-06 17:36:42 +0530

    Shared Services

---
+++ BackgroundTasks/Bookmyslot.BackgroundTasks.Api.Contracts/Configuration/AppConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Bookmyslot.Api.Common.Contracts.Constants;
+using Microsoft.Extensions.Configuration;
+
+namespace Bookmyslot.BackgroundTasks.Api.Contracts.Configuration
+{
+
+    public class AppConfiguration
+    {
+        private readonly string appVersion;
+
+        private readonly string logOutputTemplate;
+
+        private readonly string elasticSearchUrl;
+
+        public AppConfiguration(IConfiguration configuration)
+        {
+            this.appVersion = configuration.GetSection(AppSettingKeysConstants.AppVersion).Value;
+            this.logOutputTemplate = configuration.GetSection(AppSettingKeysConstants.LogSettings).GetSection(AppSettingKeysConstants.LogOutPutTemplate).Value;
+            this.elasticSearchUrl = configuration.GetSection(AppSettingKeysConstants.ElasticSearchUrl).Value;
+        }
+
+        public string AppVersion => this.appVersion;
+
+        public string LogOutputTemplate => this.logOutputTemplate;
+
+        public string ElasticSearchUrl => this.elasticSearchUrl;
+
+    }
+}
---
+++ BackgroundTasks/Bookmyslot.BackgroundTasks.Api.Injections/AppConfigurationInjection.cs
@@ -0,0 +1,22 @@
+﻿
+using Bookmyslot.Api.Common.Contracts.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookmyslot.BackgroundTasks.Api.Injections
+{
+    public class AppConfigurationInjection
+    {
+        public static void LoadInjections(IServiceCollection services, IConfiguration configuration)
+        {
+            AppInjections(services, configuration);
+        }
+        private static void AppInjections(IServiceCollection services, IConfiguration configuration)
+        {
+            var cacheConfiguration = new Contracts.Configuration.AppConfiguration(configuration);
+            services.AddSingleton(cacheConfiguration);
+            var emailConfiguration = new EmailConfiguration(configuration);
+            services.AddSingleton(emailConfiguration);
+        }
+    }
+}
